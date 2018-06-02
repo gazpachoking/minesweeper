4a15eaa3c103e241d42cc0fa66a96dcc98a83259
Chase Sterling <chase.sterling@gmail.com>
2018-06-02 11:58:52 -0400

    Timer, bomb counter, resizable, fix new game

--- minesweeper.py
+++ minesweeper.py
@@ -1,24 +1,15 @@
 from collections import namedtuple
 from itertools import chain
 from random import shuffle
+import sys
 import time
 import typing
 
 from asciimatics.event import KeyboardEvent, MouseEvent
-from asciimatics.exceptions import StopApplication
+from asciimatics.exceptions import ResizeScreenError, StopApplication
 from asciimatics.scene import Scene
 from asciimatics.screen import Screen
-from asciimatics.widgets import (
-    Frame,
-    Label,
-    ListBox,
-    Layout,
-    Divider,
-    Text,
-    Button,
-    TextBox,
-    Widget,
-)
+from asciimatics.widgets import Frame, Label, Layout, Divider, Text, Button, Widget
 import click
 
 
@@ -44,17 +35,29 @@ class Tile(object):
 
 class Board(object):
     def __init__(self, width: int, height: int, num_mines: int):
-        assert num_mines < width * height
+        self.field = []
         self.width = width
         self.height = height
         self.num_mines = num_mines
-        self.field = []
+        self.start_time = 0.0
+        self.end_time = 0.0
+        self.status = NOT_STARTED
+        self.new(width, height, num_mines)
+
+    def new(self, width: int = None, height: int = None, num_mines: int = None):
+        if width is not None:
+            self.width = width
+        if height is not None:
+            self.height = height
+        if num_mines is not None:
+            self.num_mines = num_mines
         self.place_mines(Position(0, 0))
         self.status = NOT_STARTED
         self.start_time = None
         self.end_time = None
 
     def place_mines(self, start_pos: Position):
+        assert self.num_mines < self.width * self.height
         self.field.clear()
         tiles = [
             Tile(mine=x < self.num_mines) for x in range(self.width * self.height - 1)
@@ -89,7 +92,6 @@ class Board(object):
             self.reveal_all(tile.pos)
         if self.is_lose():
             self.status = LOST
-            self.reveal_mines()
             self.end_time = time.time()
         elif self.is_win():
             self.status = WON
@@ -102,6 +104,10 @@ class Board(object):
         if self.end_time:
             return self.end_time - self.start_time
         return time.time() - self.start_time
+
+    @property
+    def mines_left(self) -> int:
+        return self.num_mines - sum(1 if t.marked else 0 for t in self.all_tiles())
 
     def reveal_all(self, pos: Position):
         self.reveal(pos)
@@ -126,11 +132,6 @@ class Board(object):
 
     def all_tiles(self) -> typing.Iterable[Tile]:
         return chain(*self.field)
-
-    def reveal_mines(self):
-        for tile in self.all_tiles():
-            if tile.mine:
-                tile.revealed = True
 
     def _in_range(self, pos: Position):
         x, y = pos
@@ -159,14 +160,24 @@ class MineField(Widget):
                 char = "#"
             elif not tile.revealed:
                 char = "░"
-            elif tile.mine:
-                color = Screen.COLOUR_MAGENTA
-                char = "*"
             elif tile.neighbors:
                 char = str(tile.neighbors)
             else:
                 char = " "
-            self._frame.canvas.print_at(char, self._x + tile.pos.x, self._y + tile.pos.y, color)
+            if self._board.status in [WON, LOST]:
+                if tile.marked and tile.mine:
+                    color = Screen.COLOUR_GREEN
+                elif tile.mine:
+                    color = Screen.COLOUR_GREEN
+                    if tile.revealed:
+                        color = Screen.COLOUR_RED
+                    char = "*"
+                elif tile.marked:
+                    color = Screen.COLOUR_RED
+                    char = "X"
+            self._frame.canvas.print_at(
+                char, self._x + tile.pos.x, self._y + tile.pos.y, color
+            )
             self._frame.canvas.print_at("aoeu", 0, 0)
 
     def reset(self):
@@ -175,14 +186,14 @@ class MineField(Widget):
     def process_event(self, event):
         if isinstance(event, KeyboardEvent):
             if event.key_code in (ord("Q"), ord("q")):
-                raise StopApplication('User Quit')
+                raise StopApplication("User Quit")
             elif event.key_code in (ord("N"), ord("n")):
-                self.new()
+                self._board.new()
         elif isinstance(event, MouseEvent):
             event = self._frame.rebase_event(event)
             if not self.is_mouse_over(event, include_label=False):
                 return
-            pos = Position(event.x-self._x, event.y-self._y)
+            pos = Position(event.x - self._x, event.y - self._y)
             if event.buttons & event.LEFT_CLICK:
                 self._board.reveal(pos)
             if event.buttons & event.RIGHT_CLICK:
@@ -204,23 +215,29 @@ class MineField(Widget):
 
 class GameBoard(Frame):
     def __init__(self, screen, board):
-        super().__init__(screen, board.height+3, board.width+2, title="Minesweeper")
-        layout1 = Layout([1, 1, 1, 1])
+        super().__init__(screen, board.height + 3, board.width + 2, title="Minesweeper")
+        self._board = board
+        layout1 = Layout([1, 1])
         self.add_layout(layout1)
-        layout1.add_widget(Label("Time"), 0)
-        layout1.add_widget(Label("0"), 1)
-        layout1.add_widget(Label("Mines"), 2)
-        layout1.add_widget(Label(str(board.num_mines)), 3)
+        self._time_label = Label("0")
+        self._mine_label = Label(str(self._board.num_mines))
+        layout1.add_widget(self._time_label, 0)
+        layout1.add_widget(self._mine_label, 1)
         self._mine_field = MineField(board)
         layout = Layout([100])
         self.add_layout(layout)
         layout.add_widget(self._mine_field)
         self.fix()
 
+    def _update(self, frame_no):
+        self._time_label.text = str(round(self._board.play_duration))
+        self._mine_label.text = str(self._board.mines_left)
+        super()._update(frame_no)
+
 
 def run_scene(screen, board):
     scenes = [Scene([GameBoard(screen, board)], -1, name="Main")]
-    screen.play(scenes)
+    screen.play(scenes, stop_on_resize=True)
 
 
 @click.command()
@@ -231,9 +248,13 @@ def main(size, mines):
         size = (15, 15)
     if not mines:
         mines = int(size[0] * size[1] * 0.15)
-    # frame = widgets.Frame(screen, screen.height, screen.width)
     board = Board(size[0], size[1], mines)
-    Screen.wrapper(run_scene, arguments=[board])
+    while True:
+        try:
+            Screen.wrapper(run_scene, arguments=[board])
+            sys.exit(0)
+        except ResizeScreenError as e:
+            pass
 
 
 if __name__ == "__main__":
